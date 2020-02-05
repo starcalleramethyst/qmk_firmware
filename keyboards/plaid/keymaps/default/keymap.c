81fd531d254d871d89bb313dda9409fcb60f0243
starcalleramethyst <toppleld@gmail.com>
2020-02-05 09:43:24 -0600

    added custome keycodes

--- keyboards/plaid/keymaps/default/keymap.c
+++ keyboards/plaid/keymaps/default/keymap.c
@@ -59,16 +59,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ESC  | RAlt | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | ESC  | RAlt | Alt  | GUI  |Lower |    Space    |Raise | /    | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_plaid_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_ESC, KC_RALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_ENT ,
+    KC_ESC, KC_RALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
 /* Lower
@@ -91,35 +91,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |   `  |SLK_H |SLK_HP| EM_O |EM_SS | EM_FU| EM_C |      |      | BIKE |TREAD | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |  1   |  2   |   3  |      |      |      |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |SCRN_C|  4   |  5   |  6   |      |      |      |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |  7   |  8   |   9  |   0  |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_plaid_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_GRV,   S_H,  S_H_P,   E_OP,    E_SS,    E_FU,    E_CL,    _______,    _______,    T_H_B,    T_H_T,    KC_BSPC,
+    KC_DEL,  KC_1,   KC_2,   KC_3,   _______,   _______,   _______,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    SCRN_C, KC_4,   KC_5,   KC_6,   _______,  _______,  _______,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    _______, KC_7,   KC_8,   KC_9, KC_0, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |Reset |      |      |      |      |      |      |      |      |      |      |  Del |
+ * |Reset | PEND | OPEN | SOLVE|      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm|AGswap|      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  RGB |RGBMOR|      |      |      |             |      |      |      |      |      |
+ * |  RGB |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_plaid_grid(
-    RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+    RESET, ZD_STAT_P, ZD_STAT_O, ZD_STAT_S, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______,  _______, _______,  _______,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
--- keyboards/plaid/keymaps/starcalleramethyst/config.h
+++ keyboards/plaid/keymaps/starcalleramethyst/config.h
@@ -18,14 +18,14 @@
 
 // place overrides here
 
-/* ws2812 RGB LED */
-#define RGB_DI_PIN C4
-#define RGBLED_NUM 30   // Number of LEDs
-#define RGBLIGHT_HUE_STEP 6
-#define RGBLIGHT_SAT_STEP 4
-#define RGBLIGHT_VAL_STEP 8
+// /* ws2812 RGB LED */
+// #define RGB_DI_PIN C4
+// #define RGBLED_NUM 30   // Number of LEDs
+// #define RGBLIGHT_HUE_STEP 6
+// #define RGBLIGHT_SAT_STEP 4
+// #define RGBLIGHT_VAL_STEP 8
 
-#define RGBLIGHT_TIMER
-#define RGBLIGHT_SLEEP
+// #define RGBLIGHT_TIMER
+// #define RGBLIGHT_SLEEP
 
-#define TAPPING_TERM 200
+// #define TAPPING_TERM 200
